51b9aaa3d08698c63bcc693c46293131838666a2
nnposter <nnposter@users.noreply.github.com>
2021-07-13 16:43:39 -0600

    Detect another title variant. See #11

--- http-default-accounts-fingerprints-nndefaccts.lua
+++ http-default-accounts-fingerprints-nndefaccts.lua
@@ -5862,7 +5862,7 @@ table.insert(fingerprints, {
            and response.body
            and response.body:find("Polycom", 1, true)
            and response.body:find("submitLoginInfo", 1, true)
-           and response.body:lower():find("<title>polycom - configuration utility</title>", 1, true)
+           and response.body:lower():find("<title>polyc?o?m? %- configuration utility</title>")
            and get_tag(response.body, "input", {name="^password$", autocomplete="^off$"})
   end,
   login_combos = {
